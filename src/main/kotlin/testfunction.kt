879bd88e590ca81a518c334cc009ebc92c64536b
건우 이 <zf0908@naver.com>
2022-12-28 23:37:13 +0900

    upload

--- src/main/kotlin/testfunction.kt
+++ src/main/kotlin/testfunction.kt
@@ -1,8 +1,14 @@
 fun main()
 {
+    var i : Int = 23;
+    println(i)
 
+    var sum : Int = addfunction(10, 30)
+    println(sum)
 }
-fun addfunction()
+fun addfunction(num1 : Int, num2 : Int) : Int
 {
-
+    var ans : Int
+    ans = num1+num2
+    return ans
 }
